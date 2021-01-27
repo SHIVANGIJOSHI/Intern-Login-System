0a65a141a2e34b11f818050c3db96859248bd7e5
Shivangi Joshi <shivangijoshi16399@gmail.com>
2021-01-27 15:35:45 +0530

    jwt-api

--- intern-login-backend/routes/api.php
+++ intern-login-backend/routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\Interns;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,9 +13,25 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+|//Hash::make('pass');
 */
 
 Route::get('/login/index','App\Http\Controllers\InternsController@index');
 Route::post('/login/authenticate','App\Http\Controllers\InternsController@authenticate');
-
+Route::post('/login/token-auth',function(){
+    $credentials = request()->only(['username','password']);
+    $token = auth()->attempt($credentials);
+    return $token;
+});
+Route::get('/login/user-create',function(){
+    User::create([
+        'First_name' => 'Shivangi',
+        'Last_name'  => 'Joshi',
+        'username' => 'sj@docquity.com',
+        'password' => Hash::make('SJoshi@7#')
+    ]);
+    return response()->json(['messg'=>'Success']);
+});
+Route::middleware('auth')->get('/me',function (){
+    return auth()->user();
+});
